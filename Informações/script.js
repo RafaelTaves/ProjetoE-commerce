85958e47fbe24707202a6e408056fe1bebac9ac1
t4vex <rafaeljosetaves@gmail.com>
2023-11-17 20:06:40 -0300

    atualizando

--- Informações/script.js
+++ Informações/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Modal Registro
 
 let modalBtn = document.querySelector('.modal-btn')
@@ -11,18 +10,4 @@ modalBtn.addEventListener('click', function(){
 })
 modalClose.addEventListener('click', function(){
   modalBg.classList.remove('bg-active')
-=======
-//Modal Registro
-
-let modalBtn = document.querySelector('.modal-btn')
-let modalBg = document.querySelector('.modal-bg')
-let modalClose = document.querySelector('.modal-close')
-
-modalBtn.addEventListener('click', function(){
-  modalBg.classList.add('bg-active')
-  event.preventDefault();
-})
-modalClose.addEventListener('click', function(){
-  modalBg.classList.remove('bg-active')
->>>>>>> 4982c55e026e43916becbfa195a3dcbc070d5102
 })
